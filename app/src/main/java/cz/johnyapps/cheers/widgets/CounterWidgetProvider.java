039091052f6d82139d0e39c18d4f192efd4655d0
Johny Da Developer <johnydadeveloper@gmail.com>
2021-09-24 23:14:29 +0200

    Work on widgets

---
+++ app/src/main/java/cz/johnyapps/cheers/widgets/CounterWidgetProvider.java
@@ -0,0 +1,19 @@
+package cz.johnyapps.cheers.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import cz.johnyapps.cheers.R;
+
+public class CounterWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_counter);
+            views.setOnClickResponse(R.id.nav_view, new RemoteViews.RemoteResponse());
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
