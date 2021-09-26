9868e63d2871e24dcbfedd9b1517ceba100c36c4
Johny Da Developer <johnydadeveloper@gmail.com>
2021-09-26 22:27:14 +0200

    Added cling sounds

--- app/src/main/java/cz/johnyapps/cheers/entities/BeverageCategory.java
+++ app/src/main/java/cz/johnyapps/cheers/entities/BeverageCategory.java
@@ -8,21 +8,24 @@ import cz.johnyapps.cheers.R;
 import cz.johnyapps.cheers.SharedPrefsNames;
 
 public enum  BeverageCategory {
-    BEER(R.string.beverage_beer, R.drawable.beer, SharedPrefsNames.BEER_COUNT),
-    WINE(R.string.beverage_wine, R.drawable.wine, SharedPrefsNames.WINE_COUNT),
+    BEER(R.string.beverage_beer, R.drawable.beer, SharedPrefsNames.BEER_COUNT, R.raw.beer1, R.raw.beer2),
+    WINE(R.string.beverage_wine, R.drawable.wine, SharedPrefsNames.WINE_COUNT, R.raw.wine1),
     SHOT(R.string.beverage_shot, R.drawable.shot, SharedPrefsNames.SHOT_COUNT);
 
     private final int titleResId;
     private final int imageResId;
     @NonNull
     private final String countPrefName;
+    private final int[] sounds;
 
     BeverageCategory(@StringRes int titleResId,
                      @DrawableRes int imageResId,
-                     @NonNull String countPrefName) {
+                     @NonNull String countPrefName,
+                     int... sounds) {
         this.titleResId = titleResId;
         this.imageResId = imageResId;
         this.countPrefName = countPrefName;
+        this.sounds = sounds;
     }
 
     public int getTitleResId() {
@@ -36,5 +39,9 @@ public enum  BeverageCategory {
     @NonNull
     public String getCountPrefName() {
         return countPrefName;
+    }
+
+    public int[] getSounds() {
+        return sounds;
     }
 }
--- app/src/main/java/cz/johnyapps/cheers/fragments/BeverageCategoryFragment.java
+++ app/src/main/java/cz/johnyapps/cheers/fragments/BeverageCategoryFragment.java
@@ -1,6 +1,8 @@
 package cz.johnyapps.cheers.fragments;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import java.util.Random;
 
 import cz.johnyapps.cheers.R;
 import cz.johnyapps.cheers.entities.BeverageCategory;
@@ -47,6 +51,26 @@ public class BeverageCategoryFragment extends Fragment {
 
         AppCompatImageView beverageCategoryImageView = root.findViewById(R.id.beverageCategoryImageView);
         beverageCategoryImageView.setImageDrawable(ContextCompat.getDrawable(root.getContext(), beverageCategory.getImageResId()));
+        beverageCategoryImageView.setOnClickListener(v -> playSound(v.getContext()));
+    }
+
+    private void playSound(@NonNull Context context) {
+        int[] sounds = beverageCategory.getSounds();
+
+        if (sounds.length == 0) {
+            return;
+        }
+
+        int soundToPlay;
+
+        if (sounds.length > 1) {
+            soundToPlay = sounds[new Random().nextInt(sounds.length)];
+        } else {
+            soundToPlay = sounds[0];
+        }
+
+        MediaPlayer player = MediaPlayer.create(context, soundToPlay);
+        player.start();
     }
 
     @NonNull
